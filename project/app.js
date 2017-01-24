17ba981d59786bd5c2a5f4c6d568078e5405329d
jeffdiers <jeff.diers@gmail.com>
2017-01-24 10:46:22 -0700

    conflicts resolved hopefully

--- project/app.js
+++ project/app.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-var index = require('./routes/index');
-var landing = require('./routes/landing');
-var users = require('./routes/users');
-var splash = require('./routes/splash');
-var brewery = require('./routes/brewery');
+const index = require('./routes/index');
+const landing = require('./routes/landing');
+const users = require('./routes/users');
+const splash = require('./routes/splash');
+const brewery = require('./routes/brewery');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
