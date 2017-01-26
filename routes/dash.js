bd63d87afa370423bc75f200f4cb026b7eaee16b
jeffdiers <jeff.diers@gmail.com>
2017-01-26 15:52:00 -0700

    dash fixed :sparkles:

--- routes/dash.js
+++ routes/dash.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const passport = require('passport');
+const db = require("../db");
+const router = require("express").Router();
+const bodyParser = require('body-parser');
+const request = require('request');
+const queries = require("../queries");
+require("../passport");
 
 router.get('/', function(req, res, next) {
   res.render('dash')
