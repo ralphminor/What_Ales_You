79df3454897ef65decf07a5ed81d3e99c95473c3
jeffdiers <jeff.diers@gmail.com>
2017-01-27 15:04:39 -0700

    updating beers

--- routes/dash.js
+++ routes/dash.js
@@ -2,12 +2,30 @@ const passport = require('passport');
 const db = require("../db");
 const router = require("express").Router();
 const bodyParser = require('body-parser');
-const request = require('request');
-const queries = require("../queries");
 require("../passport");
 
-router.get('/', function(req, res, next) {
-  res.render('dash')
-});
+function loginRequired(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
+function adminRequired(req, res, next) {
+  if (!req.user.is_admin) {
+    return res.render("403");
+  }
+  next();
+}
+
+router
+  .use(bodyParser.json())
+  .get("/", loginRequired, (req, res, next) => {
+    db("beer").then((beers) => {
+      res.render("dash", {
+        title: "All Beers",
+        beers,})
+    }, next)
+  })
 
 module.exports = router;
